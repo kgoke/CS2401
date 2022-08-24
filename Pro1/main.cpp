7b7b1344928f149d37c10f3f6dd1463ccc668c3a
Keegan Goecke <89423561+kgoke@users.noreply.github.com>
2022-08-24 19:08:58 -0400

    added project number 1

---
+++ Pro1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS_Pro1
+//
+//  Created by Keegan Goecke on 1/25/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
